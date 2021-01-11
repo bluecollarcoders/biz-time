9584206535c151abc5daa7728035eca4e9ec3b29
Caleb Matteis <matteismusic@gmail.com>
2021-01-11 09:21:48 -0500

    setup get route for companies route

--- routes/companies.js
+++ routes/companies.js
@@ -6,3 +6,18 @@ const ExpressError = require('../expressError')
 
 let router = new express.Router();
 
+// Get / => list of companies
+
+router.get('/', async function (req, res, next) {
+    try {
+        const result = await db.query(
+            `SELECT code, name
+            FROM companies
+            ORDER BY name`
+        );
+        return res.json({'companies': result.rows});
+    }
+    catch (err) {
+        return next(err);
+    }
+});
