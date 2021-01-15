32388fd3c5e41259ce32aa860bf8911201ece5b5
Caleb Matteis <matteismusic@gmail.com>
2021-01-15 07:25:51 -0500

    added test for companies route

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -25,3 +25,54 @@ describe('GET /', function () {
         });
     })
 }); 
+
+
+describe('GET /apple', function () {
+    
+    test("It returns company info", async function () {
+        const response = await request(app).get("/companies/apple");
+        expect(response.body).toEqual(
+            {
+                "company": {
+                   code: "apple",
+                   name: "Apple",
+                   description: "Maker of OSX.",
+                   invoices: [1,2],
+                }
+            }
+        );
+    });
+
+    test("It should return 404 for no such-company", async function () {
+        const response = await request(app).get("/companies/blargh");
+        expect(response.status).toEqual(404);
+    })
+});
+
+
+describe("POST /", function () {
+    
+    test("It should add company", async function () {
+        const response = await request(app)
+        .post('/companies')
+        .send({name: "TacoTime", description: "Yum!"});
+
+        expect(response.body).toEqual(
+            {
+                "company": {
+                    code: "tacotime",
+                    name: "TacoTime",
+                    description: "Yum!",
+                }
+            }
+        );
+    });
+
+    test("It should return 500 for conflict", async function () {
+        const response = await request(app)
+           .post("/companies")
+           .send({name: "Apple", description: "Huh?"});
+
+        expect(response.status).toEqual(500);
+    })
+});
